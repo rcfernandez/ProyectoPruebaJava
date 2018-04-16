2875f858c599c91b932abd9da943d0df8bccdbcf
admin <admin@192.168.0.30>
2018-04-15 23:02:01 -0300

    modificado domingo 15/04

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/ConexionBaseDeDatosTest.java
@@ -6,8 +6,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.*;
 
-// Clase que prueba la conexion a la base de datos. Hereda de SpringTest por lo que corre dentro del contexto
-// de spring
+// Clase que prueba la conexion a la base de datos. Hereda de SpringTest por lo que corre dentro del contexto de spring
 public class ConexionBaseDeDatosTest extends SpringTest{
 
     @Test
